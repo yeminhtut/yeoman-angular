8a6c406a7710b7aa04b9d87064b1e8cccd9904ac
yeminhtut <yehtut.it@gmail.com>
2016-08-19 17:46:43 +0800

    remove hash tag from url

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,7 +17,7 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider,$locationProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
@@ -32,4 +32,9 @@ angular
       .otherwise({
         redirectTo: '/'
       });
+      // use the HTML5 History API
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    })
   });
